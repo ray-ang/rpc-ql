33e3cebb81e1725c5658a72f5bc65ed75bd95e82
ray-ang <46805160+ray-ang@users.noreply.github.com>
2019-09-02 02:34:39 -0400

    Changed in_array( ) to stristr( ) case insensitive

--- rpc-ql.php
+++ rpc-ql.php
@@ -22,6 +22,8 @@ $whitelist = ['SELECT' => 'SQL',
               'FROM' => 'SQL',
               'WHERE' => 'SQL',
               'AND' => 'SQL',
+              'OR' => 'SQL',
+              'IN' => 'SQL',
               'LIKE' => 'SQL',
               ' ' => 'SQL',
               '_' => 'SQL',
@@ -36,6 +38,7 @@ $whitelist = ['SELECT' => 'SQL',
               ')' => 'SQL',
               "'" => 'SQL',
               '"' => 'SQL',
+              // Table and field names
               'persons' => 'Persons table',
               'person_id' => 'integer - The ID number of the person',
               'person_name' => 'string - Name of the person',
@@ -47,20 +50,16 @@ $whitelist = ['SELECT' => 'SQL',
               'place_state' => 'string - State where the place is located'];
 
 // Show dictionary with GET request.
-if ( $_SERVER['REQUEST_METHOD'] == 'GET' ) echo json_encode($whitelist);
+if ($_SERVER['REQUEST_METHOD'] == 'GET') echo json_encode($whitelist);
 
 // Remote API function
 function rpc_ql()
 {
 
-  global $whitelist;
-  global $jsonrpc;
-  global $method;
-  global $params;
-  global $id;
+  global $whitelist, $jsonrpc, $method, $params, $id;
 
   // Access API functionality with POST request.
-  if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // Require parameters based on JSON-RPC 2.0 and SQL
     if ( empty($jsonrpc) || empty($method) || empty($params['token']) || empty($params['query']) || empty($id) ) exit('Please set "jsonrpc", "method", "token" and "query" parameters, and request "ID".');
@@ -69,46 +68,44 @@ function rpc_ql()
     $query_data = $params['query_data'];
     
     // Token validation - token should be 12345
-    if ( ! hash_equals( hash('sha256', 12345), hash('sha256', $token) ) ) exit('Token authentication failed');
+    if ( ! hash_equals(hash('sha256', 12345), hash('sha256', $token)) ) exit('Token authentication has failed.');
     
     // Retrieve whitelist array keys
     $whitelist_keys = array_keys($whitelist);
 
-    // Prepare $query for array transformation
+    // Prepare $query for array conversion
     // Remove special characters attached to whitelisted terms from $querry array
-    $query_cleansed = str_ireplace( '%', '', $query ); // Initial conversion of attached characters.
-    $query_cleansed = str_ireplace( ',', '', $query_cleansed ); // Succeeding conversion of $query_cleansed.
-    $query_cleansed = str_ireplace( '(', '', $query_cleansed );
-    $query_cleansed = str_ireplace( ')', '', $query_cleansed );
-    $query_cleansed = str_ireplace( "'", '', $query_cleansed );
-    $query_cleansed = str_ireplace( '"', '', $query_cleansed );
+    $query_cleansed = str_replace( '%', '', $query ); // Initial conversion of attached characters.
+    $query_cleansed = str_replace( ',', '', $query_cleansed ); // Succeeding conversion of $query_cleansed.
+    $query_cleansed = str_replace( '(', '', $query_cleansed );
+    $query_cleansed = str_replace( ')', '', $query_cleansed );
+    $query_cleansed = str_replace( "'", '', $query_cleansed );
+    $query_cleansed = str_replace( '"', '', $query_cleansed );
     $query_array = explode( ' ', $query_cleansed );
 
     // Declare an error if not in whitelist dictionary
-    foreach ( $query_array as $query_array ) {
-      if ( ! in_array($query_array, $whitelist_keys) ) {
-        $error[] = $query_array;
+    foreach ($query_array as $query_array) {
+      if ( ! stristr(implode('::', $whitelist_keys), $query_array) ) {
+        $error_query[] = $query_array;
       }
     }
 
-    // Query validation - query should be alphanumeric with a few accepted characters and blacklisted SQL commands.
-    // if ( preg_match('/^[a-zA-Z0-9 _,*=()\']+$/i', $query) && ! preg_match('/(drop|insert|into|update|truncate)/i', $query) ) {
+    // Continue with conversion to database field names if without errors.
+    if ( empty($error_query) ) {
 
-    if ( empty($error) ) {
+      // Table and column field names converter
+      $mod_query = str_ireplace( 'persons', 'db_persons', $query ); // Persons table; Initial conversion of $query.
+      $mod_query = str_ireplace( 'person_id', 'db_person_id', $mod_query ); // Succeeding conversion of $mod_query.
+      $mod_query = str_ireplace( 'person_name', 'db_person_name', $mod_query );
+      $mod_query = str_ireplace( 'person_gender', 'db_person_gender', $mod_query );
+      $mod_query = str_ireplace( 'person_birth_date', 'db_person_birthdate', $mod_query );
+      $mod_query = str_ireplace( 'places', 'db_places', $mod_query ); // Places table
+      $mod_query = str_ireplace( 'place_id', 'db_place_id', $mod_query );
+      $mod_query = str_ireplace( 'place_name', 'db_place_name', $mod_query );
+      $mod_query = str_ireplace( 'place_state', 'db_place_state', $mod_query );
 
-      // Table and column fields converter
-      $mod_query = str_replace( 'persons', 'db_persons', $query ); // Persons table; Initial conversion of $query.
-      $mod_query = str_replace( 'person_id', 'db_person_id', $mod_query ); // Succeeding conversion of $mod_query.
-      $mod_query = str_replace( 'person_name', 'db_person_name', $mod_query );
-      $mod_query = str_replace( 'person_gender', 'db_person_gender', $mod_query );
-      $mod_query = str_replace( 'person_birth_date', 'db_person_birthdate', $mod_query );
-      $mod_query = str_replace( 'places', 'db_places', $mod_query ); // Places table
-      $mod_query = str_replace( 'place_id', 'db_place_id', $mod_query );
-      $mod_query = str_replace( 'place_name', 'db_place_name', $mod_query );
-      $mod_query = str_replace( 'place_state', 'db_place_state', $mod_query );
-
-      // Set $error to null if $mod_query contains a valid query.
-      if ( $mod_query !== false ) $error = null;
+      // Set $error to null if $mod_query is a valid query. (JSON-RPC 2.0)
+      if ($mod_query !== false) $error = null;
 
       /*** Perform SQL execution using PHP PDO with '?' as placeholder and $query_data OR $params['query_data'] as the array of data. ***/
       
@@ -120,7 +117,7 @@ function rpc_ql()
 
       // Failed validation
       $error_message = "The query is not valid. Please verify with whitelisted terms.";
-      $response = ['jsonrpc' => $jsonrpc, 'result' => null, 'error' => ['code' => 32600, 'message' => $error_message], 'id' => $id];
+      $response = ['jsonrpc' => $jsonrpc, 'result' => null, 'error' => ['code' => 32602, 'message' => $error_message], 'id' => $id];
       echo json_encode($response);
 
     }
@@ -134,6 +131,6 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' && function_exists($method) ) {
   return $method();
 } elseif ( $_SERVER['REQUEST_METHOD'] == 'POST' && ! function_exists($method) ) {
   $error_message = 'Sorry. The included method does not exist.';
-  $response = ['jsonrpc' => $jsonrpc, 'result' => null, 'error' => ['code' => 32602, 'message' => $error_message], 'id' => $id];
+  $response = ['jsonrpc' => $jsonrpc, 'result' => null, 'error' => ['code' => 32601, 'message' => $error_message], 'id' => $id];
   echo json_encode($response);
 }
